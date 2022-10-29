e5f5c2e40cc1073c306732a32bacef620a8d3a12
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-10-30 00:27:28 +0600

    npm run buld

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { register } from "./serviceWorker"
+import { register } from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
