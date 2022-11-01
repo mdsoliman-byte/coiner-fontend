75a86ddeba3aaceb1a21ec6aeae9719e9436c4bf
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-01 23:10:03 +0600

    service worker file testing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { register } from "./serviceWorker";
-
+import * as serviceWorkers from "./serviceWorkers";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,5 +14,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-register()
+serviceWorkers.register()
 reportWebVitals();
