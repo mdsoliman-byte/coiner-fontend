0937e3aed8fcbf68e74599c0ef92e45d2f6db84d
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-05 00:20:18 +0600

    pwa check

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import * as serviceWorkers from "../public/serviceWorkers";
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkers from "./serviceWorkers";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-serviceWorkers.register()
+// serviceWorkers.register()
 reportWebVitals();
